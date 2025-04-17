fcb07eadfaf3bf85df0dc0da4970b7c254ac9049
Raj Yadav <94789028+rajyadav282@users.noreply.github.com>
2025-04-17 18:33:28 +0530

    Update admin.js

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,22 +1,22 @@
 
-const jwt = require("jsonwebtoken")
-const {JWT_ADMIN_PASSWORD} = require("../config");
-function adminMiddleware(req , res , next){
-    const token = req.body.token;
+const jwt = require("jsonwebtoken") 
+const {JWT_ADMIN_PASSWORD} = require("../config");     
+function adminMiddleware(req , res , next){      
+    const token = req.body.token; 
     const adminverify = jwt.verify({
-        token
+        tokn   
     },JWT_ADMIN_PASSWORD)
-    
+               
     if(adminverify){
-        req.userId = adminverify.id;
-    }
-    else{
+        req.userId = adminverify.id;   } 
+        
+    else
         res.json({
             message  : "You are not signed IN"
         })
     }
 }
-
+    
 module.exports = {
-    adminMiddleware
-}
+     adminMiddleware
+}  
